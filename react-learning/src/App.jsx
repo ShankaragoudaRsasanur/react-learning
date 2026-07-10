5cf04f93635a7ab8f9bc7624107d819cbe47dbf4
ShankaragoudaRsasanur <shankargoudasasanur34@gmail.com>
2026-07-10 16:45:22 +0530

    first React commit

---
+++ react-learning/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello I am Shankaragouda</h1>
+      <p>I am learning react.js classes</p>
+      <button style={{backgroundColor:'green'}}> click me</button>
+    </>
+  )
+}
+
+export default App
