818102985fb8b69535881e31549d20696968f95b
ShankaragoudaRsasanur <shankargoudasasanur34@gmail.com>
2026-07-11 20:55:03 +0530

    Updated React learning code

---
+++ react-learning/src/topics/JSX.jsx
@@ -0,0 +1,41 @@
+// jsx/// 
+// it is javascript xml and it allows us write the html like code inside the javascript  
+
+// it is syntax extension  for javascript that allows us to write the html like syntax inside the javascript   
+//    javascript+html like syntax 
+// syntax for jsx
+// function componentName(){
+// return (
+//     <div></div>
+//     <h1></h1>
+// )
+//  }
+
+// App component->return statement excuteded->jsx describes the ui->react display the ui -> we see the output in broweser.
+// why  we need the jsx because of without jsx the react element can be harder to read 
+// jsx is easer to read and write 
+// jsx is must have one parent element
+// fragments  it is groups multiple jsx elements and without creating an extera html elements in the dom
+//  example  the jsx  with help  can we create the port poli
+// function App() {
+//   const name = "Shankaragouda";
+//   const role = "Frontend Developer";
+
+//   return (
+//     <div>
+
+//       <h1>{name}</h1>
+
+//       <h2>{role}</h2>
+
+//       <p>
+//         I build responsive and interactive websites.
+//       </p>
+
+//       <button>View My Projects</button>
+
+//     </div>
+//   );
+// }
+
+// export default App;//
