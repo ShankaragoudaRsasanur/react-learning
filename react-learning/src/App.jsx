e9810b53e2f0cfde7128d1e69aafe3db97546707
ShankaragoudaRsasanur <shankargoudasasanur34@gmail.com>
2026-07-19 13:29:51 +0530

    login eample

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -354,4 +354,15 @@ Syntax
 
 condition ? truePart : falsePart
 
+example on login 
+function App() {
+  let isLoggedIn = true;
 
+  return (
+    <div>
+      {isLoggedIn ? <h1>Welcome</h1> : <h1>Please Login</h1>}
+    </div>
+  );
+}
+
+export default App;
